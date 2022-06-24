77ffa9d599fa63cd71f1801a6f79aae5fe4363da
Zack Lott <zack@zacklott.co.uk>
2022-06-24 12:55:16 +0100

    Use the new struct to build the tables

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/jedib0t/go-pretty/v6 v6.3.3
 
 require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
 	github.com/enescakir/emoji v1.0.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
--- transport/commands/scan_security_headers.go
+++ transport/commands/scan_security_headers.go
@@ -1,9 +1,10 @@
 package commands
 
 import (
-	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/common-nighthawk/go-figure"
 	"github.com/roadsigns/http-header-scanner/actions"
-	"os"
+	"github.com/roadsigns/http-header-scanner/services"
+	"time"
 )
 
 func ScanSecurityHeaders(args []string) {
@@ -18,33 +19,10 @@ func ScanSecurityHeaders(args []string) {
 
 	results := actions.CheckSecurityHttpHeaders(domain)
 
-	// Create an Interface to write table so if package dies
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Http Header", "Reason"})
-	for _, header := range results.MissingHeaders {
-		t.AppendRow([]interface{}{header.Title, header.Reason})
-		t.AppendSeparator()
-	}
-	t.Render()
-
-	if len(results.AdditionalInformationHeader) > 0 {
-		t = table.NewWriter()
-		t.SetOutputMirror(os.Stdout)
-		t.AppendHeader(table.Row{"Http Header", "Reason"})
-		for _, header := range results.AdditionalInformationHeader {
-			t.AppendRow([]interface{}{header.Title, header.Reason})
-			t.AppendSeparator()
-		}
-		t.Render()
-	}
-
-	t = table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Http Header", "Content"})
-	for _, header := range results.RawHeaders {
-		t.AppendRow([]interface{}{header.Title, header.Content})
-		t.AppendSeparator()
-	}
-	t.Render()
+	figure.NewFigure("HTTP Header Scanner", "", true).Print()
+	tableGenerator := services.CommandLineTableGenerator{}
+	tableGenerator.OutputGenericInformationTable(domain.Domain, time.Now().Format("15:04:05 01 Jan 2006"))
+	tableGenerator.OutputRawHttpHeaderTable(results.RawHeaders)
+	figure.NewFigure("Missing Security Headers", "", true).Print()
+	tableGenerator.OutputMissingHeaderTable(results.MissingHeaders)
 }
