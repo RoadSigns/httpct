0ca6bca0bb0fed6b92bbd538c5d0493611476b09
Zack Lott <zack@zacklott.co.uk>
2022-06-24 18:12:10 +0100

    Hide the missing security headers if none are found

--- transport/commands/scan_security_headers.go
+++ transport/commands/scan_security_headers.go
@@ -32,7 +32,9 @@ func ScanSecurityHeaders(args []string) int {
 	tableGenerator := services.CommandLineTableGenerator{}
 	tableGenerator.OutputGenericInformationTable(domain.Domain, time.Now().Format("15:04:05 01 Jan 2006"))
 	tableGenerator.OutputRawHttpHeaderTable(results.RawHeaders)
-	figure.NewFigure("Missing Security Headers", "", true).Print()
-	tableGenerator.OutputMissingHeaderTable(results.MissingHeaders)
+	if len(results.MissingHeaders) > 0 {
+		figure.NewFigure("Missing Security Headers", "", true).Print()
+		tableGenerator.OutputMissingHeaderTable(results.MissingHeaders)
+	}
 	return 0
 }
