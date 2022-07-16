527aafbc7f0188e310bfdd52905ead2e8099f98f
Zack <zack@zacklott.co.uk>
2022-07-16 17:48:13 +0100

    Introduce Content Security Policy

--- services/securityheaderscanner/check_for_x_xss_protection.go
+++ services/securityheaderscanner/check_for_x_xss_protection.go
@@ -5,7 +5,7 @@ func checkForXXssProtection(headers Headers) SecurityHeader {
 		Title:   "X-Xss-Protection",
 		Exists:  false,
 		Content: "",
-		Reason:  "X-Xss-Protection response header is a feature of Internet Explorer, Chrome and Safari that stops pages from loading when they detect reflected cross-site scripting (XSS) attacks.",
+		Reason:  "X-XSS-Protection sets the configuration for the XSS Auditor built into older browsers. The recommended value was \"X-XSS-Protection: 1; mode=block\" but you should now look at Content Security Policy instead.",
 		Valid:   false,
 		Guide:   "https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection",
 	}
--- services/securityheaderscanner/security_header_scanner.go
+++ services/securityheaderscanner/security_header_scanner.go
@@ -5,7 +5,7 @@ func Scan(headers Headers) []SecurityHeader {
 	var securityHeaders []SecurityHeader
 	securityHeaders = append(securityHeaders, checkForXFrameOptions(headers))
 	securityHeaders = append(securityHeaders, checkForXXssProtection(headers))
-	//securityHeaders = append(securityHeaders, checkForContentSecurityPolicy(headers))
+	securityHeaders = append(securityHeaders, checkForContentSecurityPolicy(headers))
 	//securityHeaders = append(securityHeaders, checkForStrictTransportSecurity(headers))
 	return securityHeaders
 }
